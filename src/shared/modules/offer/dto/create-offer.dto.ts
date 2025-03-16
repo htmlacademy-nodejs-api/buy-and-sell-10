f44593c64d18e03cebf08bfca4ff5c0ad9051db4
Igor Antonov <antonov.igor.khv@gmail.com>
2025-03-16 17:41:04 +0300

    4.31. Добавит `CreateOfferDTO`.
    При подготовке DTO для создания нового объявления следует учитывать, что для полей, которые ссылаются на документы из других коллекций, должен храниться идентификатор. Мы знаем, что идентификатор
    представляет собой объект типа `ObjectId`, но в DTO это будет обычная строка. Поэтому для `categories` и `userId` в качестве типа указываем
    `string`.

---
+++ src/shared/modules/offer/dto/create-offer.dto.ts
@@ -0,0 +1,11 @@
+import { OfferType } from '../../../types/index.js';
+
+export class CreateOfferDto {
+  public title: string;
+  public description: string;
+  public postDate: Date;
+  public type: OfferType;
+  public price: number;
+  public categories: string[];
+  public userId: string;
+}
